1231141ef52ec15f89c56fbf3acef3518ac4ef03
Azim Atif <azim.atif2006@gmail.com>
2024-10-29 16:53:11 -0500

    Add project files.

---
+++ Rock_Paper_Scissor/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void) {
+
+
+
+	return 0;
+}
+
+
+void RPSfunction() {
+
+
+
+}
+
---
+++ Rock_Paper_Scissor/main.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void RPSfunction(); //rock paper scissors function
