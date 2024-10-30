7b9a3cbe4599499d9572f00aea3047dbd0a33bd6
Azim Atif <azim.atif2006@gmail.com>
2024-10-29 19:28:02 -0500

    Commit#3: All tests cases

--- RPS_UnitTest/RPS_UnitTest.cpp
+++ RPS_UnitTest/RPS_UnitTest.cpp
@@ -17,15 +17,113 @@ namespace RPSUnitTest
 		{//all scenarios where Player 1 wins
 
 			const char* P1 = "rock";
-			const char* P1 = "Rock"; 
-			const char* P2 = "Scissors";
 			const char* P2 = "scissors";
 
 			const char* results = RPSfunction(P1, P2);
 
-			Assert::AreEqual("Player 1 Wins!!", results);
+			Assert::AreEqual("Player 1 Wins", results);
 
 
 		}
+		TEST_METHOD(TestP1Wins02)
+		{
+
+			const char* P1 = "paper";
+			const char* P2 = "rock";
+
+			const char* results = RPSfunction(P1, P2);
+
+			Assert::AreEqual("Player 1 Wins", results);
+
+
+		}
+		TEST_METHOD(TestP1Wins03)
+		{
+
+			const char* P1 = "scissors";
+			const char* P2 = "paper";
+
+			const char* results = RPSfunction(P1, P2);
+
+			Assert::AreEqual("Player 1 Wins", results);
+
+
+		}
+		TEST_METHOD(TestP2Wins01)
+		{//all scenarios where Player 2 wins
+
+			const char* P1 = "paper";
+			const char* P2 = "scissors";
+
+			const char* results = RPSfunction(P1, P2);
+
+			Assert::AreEqual("Player 2 Wins", results);
+
+
+		}
+		TEST_METHOD(TestP2Wins02)
+		{
+
+			const char* P1 = "rock";
+			const char* P2 = "paper";
+
+			const char* results = RPSfunction(P1, P2);
+
+			Assert::AreEqual("Player 2 Wins", results);
+
+
+		}
+		TEST_METHOD(TestP2Wins03)
+		{
+
+			const char* P1 = "scissors";
+			const char* P2 = "rock";
+
+			const char* results = RPSfunction(P1, P2);
+
+			Assert::AreEqual("Player 2 Wins", results);
+
+
+		}
+		TEST_METHOD(TestDraw01)
+		{//all scenarios where both players draw
+
+			const char* P1 = "scissors";
+			const char* P2 = "scissors";
+		
+
+			const char* results = RPSfunction(P1, P2);
+
+			Assert::AreEqual("Draw", results);
+
+
+		}
+		TEST_METHOD(TestDraw02)
+		{
+
+			const char* P1 = "rock";
+			const char* P2 = "rock";
+
+
+			const char* results = RPSfunction(P1, P2);
+
+			Assert::AreEqual("Draw", results);
+
+
+		}
+		TEST_METHOD(TestDraw03)
+		{
+
+			const char* P1 = "paper";
+			const char* P2 = "paper";
+
+
+			const char* results = RPSfunction(P1, P2);
+
+			Assert::AreEqual("Draw", results);
+
+
+		}
+
 	};
 }
